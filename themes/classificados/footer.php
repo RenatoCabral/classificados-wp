d475492b62037f113e7bf4ff449e99154775df31
RenatoCabral <carvalho.renato6@gmail.com>
2017-09-24 00:04:23 -0300

    alterado o footer para cards

--- themes/classificados/footer.php
+++ themes/classificados/footer.php
@@ -1,40 +1,62 @@
-<footer class="page-footer z-depth-2 grey lighten-3">
+<footer class="page-footer footer-classificados">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col l4 s12">
-                <h5 class="black-text title_footer">Sobre nós</h5>
-                <p class="black-text text-lighten-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-                    dolor euismod nisl ultrices, eu pulvinar nulla tristique. In quam enim, imperdiet
-                    ut enim at, volutpat finibus urna.
-                </p>
+        <div class="row footer-row">
+            <!--Sobre nos-->
+            <div class="col l4 s12 m6">
+                <div class="card z-depth-3 cards_footer">
+                    <div class="card-content white-text">
+                        <h5 class="black-text title_footer">Sobre nós</h5>
+                        <p class="black-text text-lighten-4">I am a very simple card. I am good at
+                            containing small bits of information.
+                            I am convenient because I require little markup to use effectively.
+                            Cum morbi proident, adipisicing anim corporis nunc ipsum. Nonummy
+                            sociosqu, pellentesque semper, urna. Eveniet, purus dictum quaerat
+                            exercitation congue adipisicing risus praesentium quam aliqua occaecati.
+                        </p>
+                    </div>
+                </div>
             </div>
-            <div class="col l4 s12">
-                <h5 class="black-text title_footer">Serviços</h5>
-                <ul>
-                    <li><a class="black-text text-lighten-3" href="#!">Comprar</a></li>
-                    <li><a class="black-text text-lighten-3" href="#!">Vender</a></li>
-                    <li><a class="black-text text-lighten-3" href="#!">Financiamento</a></li>
-                    <li><a class="black-text text-lighten-3" href="#!">Seguro</a></li>
-                </ul>
+            <div class="col l4 s12 m6">
+                <div class="card z-depth-3 cards_footer">
+                    <div class="card-content white-text">
+                        <h5 class="black-text title_footer">Serviços</h5>
+                        <ul>
+                            <li><a class="black-text text-lighten-3" href="#!">Comprar</a></li>
+                            <li><a class="black-text text-lighten-3" href="#!">Vender</a></li>
+                            <li><a class="black-text text-lighten-3" href="#!">Financiamento</a></li>
+                            <li><a class="black-text text-lighten-3" href="#!">Seguro</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="col l4">
-                <h5 class="black-text title_footer">Avaliação Fipe</h5>
-                <ul>
-                    <li><a class="black-text text-lighten-3" href="#!">Link 1</a></li>
-                    <li><a class="black-text text-lighten-3" href="#!">Link 2</a></li>
-                    <li><a class="black-text text-lighten-3" href="#!">Link 3</a></li>
-                    <li><a class="black-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
+            <div class="col l4 s12 m6">
+                <div class="card z-depth-3 cards_footer">
+                    <div class="card-content white-text">
+                        <h5 class="black-text title_footer">Serviços</h5>
+                        <ul>
+                            <li><a class="black-text text-lighten-3" href="#!">Comprar</a></li>
+                            <li><a class="black-text text-lighten-3" href="#!">Vender</a></li>
+                            <li><a class="black-text text-lighten-3" href="#!">Financiamento</a></li>
+                            <li><a class="black-text text-lighten-3" href="#!">Seguro</a></li>
+                        </ul>
+                    </div>
+                </div>
+<!--                <h5 class="black-text title_footer">Avaliação Fipe</h5>-->
+<!--                <ul>-->
+<!--                    <li><a class="black-text text-lighten-3" href="#!">Link 1</a></li>-->
+<!--                    <li><a class="black-text text-lighten-3" href="#!">Link 2</a></li>-->
+<!--                    <li><a class="black-text text-lighten-3" href="#!">Link 3</a></li>-->
+<!--                    <li><a class="black-text text-lighten-3" href="#!">Link 4</a></li>-->
+<!--                </ul>-->
             </div>
         </div>
     </div>
-    <div class="footer-copyright  grey lighten-3 z-depth-0">
-        <div class="container-fluid black-text">
-            © 2014 Copyright Text
-            <a class="black-text text-lighten-4 right" href="#!"></a>
-        </div>
-    </div>
+<!--    <div class="footer-copyright  grey lighten-3 z-depth-0">-->
+<!--        <div class="container-fluid black-text">-->
+<!--            © 2014 Copyright Text-->
+<!--            <a class="black-text text-lighten-4 right" href="#!"></a>-->
+<!--        </div>-->
+<!--    </div>-->
 </footer>
 
 
