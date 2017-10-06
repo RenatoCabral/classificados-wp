c4d13724811f1e5eb7de0229b17a621d9b9f4c0e
Flavia Amaral <flavia@idx.is>
2017-10-05 21:48:36 -0300

    add page

---
+++ themes/classificados/page.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col s12 m12 l12">
+			<div class="col s12 m12 l6 doubts-title">
+				<div class="row ">
+					<!--Pegar o título da página-->
+					<h1><?php the_title(); ?></h1>
+
+					<!--Esse trecho de código verifica se existe post, caso tenha
+					irá exibir o post e o conteudo-->
+					<?php if(have_posts()){
+						while (have_posts()){
+							the_post();
+							the_content();
+						}
+						wp_reset_postdata();
+
+					}?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer();?>
